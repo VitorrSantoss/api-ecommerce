9e405597f239c71d48f3418c223168e5ab3ed5a5
Vitor Santos <Vitorvitorsantos2015@hotmail.com>
2025-10-04 14:06:25 -0300

    feat: adiciona a classe Role para gerenciamento de permissoes

---
+++ src/main/java/br/com/vitorrsantoss/ecommerce_api/models/Role.java
@@ -0,0 +1,10 @@
+package br.com.vitorrsantoss.ecommerce_api.models;
+
+public enum Role { // Nossos "crachás" disponíveis no sistenma
+
+  ROLE_ADMIM,
+  ROLE_CLIENTE
+
+
+  // O prefixo "ROLE_" é uma convenção do Spring Security.
+}
